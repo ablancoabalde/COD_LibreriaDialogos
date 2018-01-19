54e0499b633eeb2fa6df8de105db118c3b834e70
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2018-01-19 09:06:12 +0100

    Mensaje básico realizado

--- src/com/alberto/dialogos/Dialogos.java
+++ src/com/alberto/dialogos/Dialogos.java
@@ -1,12 +1,14 @@
 /*
 Libreria, para tratar el tema de dialogos y confirmaciones
-*/
+ */
 package com.alberto.dialogos;
 
 import javax.swing.JOptionPane;
 
 public class Dialogos {
-    
 
-    
+    public static void mensajesimple() {
+        JOptionPane.showMessageDialog(null, "Operación realizada correctamente");
+    }
+
 }
