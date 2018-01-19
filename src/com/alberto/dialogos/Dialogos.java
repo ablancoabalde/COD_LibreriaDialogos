999956205fc74e52a1c6deb0bc679f1f2ee72ace
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2018-01-19 09:13:12 +0100

    Mensaje de confirmacion realizado

--- src/com/alberto/dialogos/Dialogos.java
+++ src/com/alberto/dialogos/Dialogos.java
@@ -6,9 +6,14 @@ package com.alberto.dialogos;
 import javax.swing.JOptionPane;
 
 public class Dialogos {
-
+    // Mensaje de acción simple
     public static void mensajesimple() {
         JOptionPane.showMessageDialog(null, "Operación realizada correctamente");
+    }
+    //Mensaje de confirmacion que devuelve un int, en el que 0= si y 1=no
+    //@return
+    public static int mensajeconfirm() {
+        return JOptionPane.showConfirmDialog(null, "¿Esta seguro?","Alerta",JOptionPane.YES_NO_OPTION);
     }
 
 }
