5da79400222092d18b91b72cd9ffe82c27cce4d9
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2018-01-19 09:28:57 +0100

    Metodo para la introduccion y devolucion de datos

--- src/com/alberto/dialogos/Dialogos.java
+++ src/com/alberto/dialogos/Dialogos.java
@@ -6,14 +6,24 @@ package com.alberto.dialogos;
 import javax.swing.JOptionPane;
 
 public class Dialogos {
+
     // Mensaje de acción simple
     public static void mensajesimple(String cadena) {
         JOptionPane.showMessageDialog(null, cadena);
     }
+
     //Mensaje de confirmacion que devuelve un int, en el que 0= si y 1=no
     //@return int
     public static int mensajeconfirm(String cadena) {
-        return JOptionPane.showConfirmDialog(null, cadena,"Alerta",JOptionPane.YES_NO_OPTION);
+        return JOptionPane.showConfirmDialog(null, cadena, "Alerta", JOptionPane.YES_NO_OPTION);
+    }
+
+    //Introducimos por pantalla un dato y lo muestra por pantalla
+    //@param respuesta de lo que introduces
+    public static void introducirdatos(String cadena) {
+        String respuesta=JOptionPane.showInputDialog(cadena);
+        JOptionPane.showMessageDialog(null, respuesta);
+
     }
 
 }
